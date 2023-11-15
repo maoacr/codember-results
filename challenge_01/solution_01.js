6fa6ebb44b371e667f4a4802c4df717776c10c22
Mao <maoacr@gmail.com>
2023-11-14 20:18:54 -0500

    Base del ejercicio 1

---
+++ challenge_01/solution_01.js
@@ -0,0 +1,11 @@
+import { message } from "./message_01";
+
+let array = message.toLowerCase().split(" ");
+
+function countWords(arr) {
+  let result = "";
+  for (let i = 0; i <= arr.length; i++) {
+    arr[i];
+  }
+  return "Estoy listo para resolverlo";
+}
