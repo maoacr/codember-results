6a102b158341fcff3c1f528dfe101e2e5e37c404
Mao <maoacr@gmail.com>
2023-11-15 11:46:37 -0500

    Solucion del segundo reto

--- challenge_02/solution_02.js
+++ challenge_02/solution_02.js
@@ -3,7 +3,20 @@ import { message } from "../challenge_01/message_01";
 let code = message.split("");
 
 function compiler(data) {
-  console.log(data);
+  let currentValue = 0;
+  let finalValue = "";
+  data.map((symbol) => {
+    if (symbol === "#") {
+      currentValue += 1;
+    } else if (symbol === "@") {
+      currentValue -= 1;
+    } else if (symbol === "*") {
+      currentValue *= currentValue;
+    } else if (symbol === "&") {
+      finalValue += currentValue;
+    }
+  });
+  return finalValue;
 }
 
 compiler(code);
